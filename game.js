52ffdd7668020efecefdd333241e7cc42e1ef02f
Gamattowicz <p.romanczuk31@gmail.com>
2021-01-05 19:49:24 +0100

    add player function

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 //PLAYER CHOOSE
-
-const btn = document.querySelector('button.start');
-
-const playerChoose = () => {
-
+const selection = {
+    player = '',
+    ai = '',
 }
 
-btn.addEventListener('click', playerChoose);
+const btn = document.querySelector('button.start');
+const choices = [...document.querySelectorAll('div.select img')];
+
+function playerChoose() {
+    selection.player = this.dataset.option;
+    choices.forEach(choice => choice.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 6px yellow';
+}
+
+choices.forEach(choice => choice.addEventListener('click', playerChoose));
